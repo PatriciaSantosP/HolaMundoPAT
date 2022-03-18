3630fc7876264eac8e975cb427be7b115d439cb6
PatriciaSantosP <thaynapatricia.santos.fp@iescampanillas.com>
2022-03-18 20:59:17 +0100

    commit 1

---
+++ app/src/main/java/com/example/miapp_holamundo/SaludoActivity.kt
@@ -0,0 +1,23 @@
+package com.example.miapp_holamundo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class SaludoActivity : AppCompatActivity() {
+    private lateinit var txtSaludo : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saludo)
+
+        //Obtenemos una referencia a los controles de la interfaz
+        txtSaludo = findViewById(R.id.txtSaludo)
+
+        //Recuperamos la información pasada en el intent
+        val saludo = intent.getStringExtra("NOMBRE")
+
+        //Construimos el mensaje a mostrar
+        txtSaludo.text = "Hola, $saludo. ¡Esta es mi primera app!"
+    }
+}
